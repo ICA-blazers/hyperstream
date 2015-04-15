959ea0f2b3807010cf0f98f78274e674992b1d51
Brenden Carney <bcarney@student.icablazers.org>
2015-04-15 08:59:18 -0500

    Poll

--- pollCollectorBackup.php
+++ pollCollectorBackup.php
@@ -1,10 +1,10 @@
 <?php
 $vote = $_REQUEST['vote'];
 
-$file = "pollResults.txt";
-$results = file($file);
+$filename = "pollResults.txt";
+$results = file($filename);
 
-$array = explode('||',$results[0]);
+$array = explode("||",$results[0]);
 $o1 = $array[0];
 $o2 = $array[1];
 $o3 = $array[2];
@@ -12,21 +12,31 @@ $o4 = $array[3];
 
 if ($vote == 0){
 $o1 = $o1 + 1;
-}
+};
 if ($vote == 1){
 $o2 = $o2 + 1;
-}
+};
 if ($vote == 2){
 $o3 = $o3 + 1;
-}
+};
 if ($vote == 3){
 $o4 = $o4 + 1;
-}
+};
 
-$newresults = $o1.'||'.$o2.'||'.$o3.'||'.$o4;
-$f = fopen($file, 'w');
+$newresults = $o1."||".$o2."||".$o3."||".$o4;
+$f = fopen($filename, 'w');
 fputs($f,$newresults);
 fclose($f);
+
+$voteTotal = ($o1+$o2+$o3+$o4);
+$votes1 = $o1;
+$votes2 = $o2;
+$votes3 = $o3;
+$votes4 = $o4;
+$percent1 = round($o1/$voteTotal,2);
+$percent2 = round($o2/$voteTotal,2);
+$percent3 = round($o3/$voteTotal,2);
+$percent4 = round($o4/$voteTotal,2);
 ?>
 
 <h2>Result:</h2>
@@ -34,29 +44,29 @@ fclose($f);
 <tr>
 <td>Mudkip:</td>
 <td>
-<img src='images/pollBar.gif' width='<?php echo(100*round($o1/($o1+$o2+$o3+$o4),2)); ?>' height = '20'>
-<?php echo(100*round($o1/($o1+$o2+$o3+$o4),2)); ?>%
+<img src="images/pollBar.gif" width='<?php echo $votes1; ?>%' height = "20">
+<?php echo $percent1; ?>%
 </td>
 </tr>
 <tr>
 <td>Bidoof:</td>
 <td>
-<img src='images/pollBar.gif' width='<?php echo(100*round($o2/($o1+$o2+$o3+$o4),2)); ?>' height = '20'>
-<?php echo(100*round($o2/($o1+$o2+$o3+$o4),2)); ?>%
+<img src="images/pollBar.gif" width="<?php echo $votes2; ?>" height = "20">
+<?php echo $percent2; ?>%
 </td>
 </tr>
 <tr>
 <td>Cyndaquil:</td>
 <td>
-<img src='images/pollBar.gif' width='<?php echo(100*round($o3/($o1+$o2+$o3+$o4),2)); ?>' height = '20'>
-<?php echo(100*round($o3/($o1+$o2+$o3+$o4),2)); ?>%
+<img src="images/pollBar.gif" width="<?php echo $votes3; ?>" height = "20">
+<?php echo $percent3; ?>%
 </td>
 </tr>
 <tr>
 <td>Will McGrath:</td>
 <td>
-<img src='images/pollBar.gif' width='<?php echo(100*round($o4/($o1+$o2+$o3+$o4),2)); ?>' height = '20'>
-<?php echo(100*round($o4/($o1+$o2+$o3+$o4),2)); ?>%
+<img src="images/pollBar.gif" width="<?php echo $votes4; ?>" height = "20">
+<?php echo $percent4; ?>%
 </td>
 </tr>
 </table>
